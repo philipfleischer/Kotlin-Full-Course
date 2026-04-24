c23e69551ef6215bce1fa57c7c37703d1c825463
philipfleischer <fleischer1@live.no>
2026-04-24 08:12:17 +0200

    Variable project completed

---
+++ Variables/src/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val me: String = "Philipef"
+    println("Hello, $me!")
+    println("Write your name: ")
+    var you: String = readln()
+    println("Hello, $you!")
+    you = you + " SvenneBy"
+    println("Hello, $you!")
+    you = "$you SvenneBy"
+    println("Hello, $you!")
+
+    var userName: String = "POPO"
+    userName = "PPolice"
+    var age: Int = 22
+    age = 234
+    println("Hello, $userName you age is $age!")
+}
